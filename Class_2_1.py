bf1291aacaedd7ed3299333ec656dfc3eb63fbdb
DESKTOP-4CO1JEL\petra <petra.budavari@gmail.com>
2024-02-01 11:45:55 -0500

    adding file from personal laptop to see if it works

---
+++ Class_2_1.py
@@ -0,0 +1,5 @@
+### always comment on your code
+## ask user Energy level and output floating pt value
+n = input("What every level of H would you like?")
+energy=13.6/int(n)*2
+print("the energy of this level is ", energy)
